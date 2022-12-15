f8555257a12e7c9d8207e4e9da42ab6da7cf8bb2
Малыгин Егор <malygin.mag@yandex.ru>
2022-12-15 10:37:33 +0500

    Revert "Revert "Merge branch 'master' of https://github.com/AfanasiyTheCat/irtctt""
    This reverts commit 762f7b22d77fa12c4b48bb3f8fec6c6dba94f17d.

--- metrics.py
+++ metrics.py
@@ -1,31 +1,38 @@
+from irtctt.irt import rasch
+from scipy.stats import zscore
+from scipy.stats import chisquare
+from psython import cronbach_alpha_scale_if_deleted
 import numpy as np
 import pandas as pd
-from .helpers import rasch_to_coef
-from .irt import rasch
-from pingouin import cronbach_alpha as p_cronbach_alpha
+from . import imath
 
-def metrics(test_results):
+def metrics(test_results, task_weights = None):
+    if task_weights is None:
+      task_weights = np.ones((test_results.shape[1]))
+    max_score = np.sum(task_weights)
+    item_total_corr, alpha_cronbach = item_total_alpha_cronbach(test_results)
+    chisquare, p = chisquare_p_metric(test_results)
     arr = {
-        # 'Delta Ferguson': delta_ferguson(test_results),
-        # 'Average Rasch': rasch_metric(test_results)
-        'Дельта Фергюсона': delta_ferguson(test_results),
-        'Среднее отклонение Раша': rasch_metric(test_results),
+        'Дельта Фергюсона': delta_ferguson(test_results, max_score),
         'KR 20': kr_20(test_results),
-        "Альфа Кронбаха": cronbach_alpha(test_results)
+        "Item Total Correlation": item_total_corr,
+        "Альфа Кронбаха": alpha_cronbach,
+        'Z-показатель': zscore_metric(test_results),
+        'Хи-квадрат': chisquare,
+        'P-показатель': p,
     }
     return arr
 
-def delta_ferguson(test_result: np.ndarray):
+def delta_ferguson(test_result: np.ndarray, max_score = None):
     shape = np.shape(test_result)
+    if not max_score:
+      max_score = shape[1]
     N = shape[0]
-    n = shape[1]
-    test_result_score = np.sum(test_result, axis=1)
+    n = max_score
+    test_result_score = np.round(np.sum(test_result, axis=1) / max_score * 100)
     unique, counts = np.unique(test_result_score, return_counts=True)
     counts = np.sum(np.power(counts, 2))
     return (n+1)*(N*N - counts)/(n*N*N)
-
-def rasch_metric(test_result):
-    return np.mean(rasch_to_coef(rasch(test_result)))
 
 #Метод Кьюдера—Ричардсона KR-20
 def kr_20(test):
@@ -37,6 +44,14 @@ def kr_20(test):
     r = M / (M - 1) * (1 - (sum(x * (1-x) for x in p)) / sx2)
     return r
 
-def cronbach_alpha(test_results):
-    return p_cronbach_alpha(pd.DataFrame(test_results))[0]
-    
+def zscore_metric(test_result):
+  return np.abs(np.mean(np.nan_to_num(zscore(test_result, ddof=0, nan_policy='propagate'))))
+
+def item_total_alpha_cronbach(test_results):
+  d = cronbach_alpha_scale_if_deleted(pd.DataFrame(test_results))[1]
+  return np.mean(np.nan_to_num(d['Corrected Item-Total Correlation'].values)), np.mean(d["Cronbach's Alpha if Item Deleted"].values)
+
+def chisquare_p_metric(test_result):
+  d = chisquare(test_result)
+  chisquare_v = np.mean(np.nan_to_num(d[0])) / 100
+  return chisquare_v, np.mean(np.nan_to_num(d[1]))
